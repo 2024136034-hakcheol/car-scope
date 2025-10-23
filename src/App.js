62a48149d2e2763549794540d64cc9baa4928f9c
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-10-23 15:10:46 +0900

    Text test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          텍스트 테스트 입니다.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
