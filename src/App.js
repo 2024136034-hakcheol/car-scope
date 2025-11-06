98670edcfc0c92277ea82feb149cffec2a880533
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-06 20:09:34 +0900

    feat(header): App.js에 App.css import 추가 및 최종 레이아웃 CSS 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
