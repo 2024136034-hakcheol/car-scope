3e93d117ce9fba0e520e991e853750a895412e0d
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-06 19:41:10 +0900

    feat: ParkingPage에서 문제 발생 요소 (주차장 목록) 전체 삭제 및 CSS 정리

--- src/pages/ParkingPage.js
+++ src/pages/ParkingPage.js
@@ -17,32 +17,7 @@ const ParkingPage = () => {
                     </div>
                 </div>
 
-                <h2>이런 주차장은 어떠세요?</h2>
-                
-                <div className="recommendation-list">
-                    {/* item-grid-card에 position: relative 적용 예정 */}
-                    <div className="item-grid-card">
-                        <div className="item-details">
-                            <p className="item-name">카카오 T AK플라자 분당 주차장 이름이 아주 긴 경우 테스트</p>
-                            <p className="item-info">377건 · 32m</p>
-                        </div>
-                        {/* item-right-section에 position: absolute 적용 예정 */}
-                        <div className="item-right-section">
-                            <div className="item-price">심야권 4,000원 외 1개</div>
-                            <button className="parking-reserve-btn">주차장 예약하기</button>
-                        </div>
-                    </div>
-                    <div className="item-grid-card">
-                        <div className="item-details">
-                            <p className="item-name">아반포트 서현265 주차장</p>
-                            <p className="item-info">273건 · 101m</p>
-                        </div>
-                        <div className="item-right-section">
-                            <div className="item-price">심야권 2,900원 외 1개</div>
-                            <button className="parking-reserve-btn">주차장 예약하기</button>
-                        </div>
-                    </div>
-                </div>
+                {/* 주차장 추천 목록 삭제 */}
 
                 <div className="review-list-section">
                     <h2>리뷰 리스트</h2>
