c894c99912f3159ebd4ec61551e78ca4011d0077
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-13 16:25:48 +0900

    fix: App.js fully cleaned and all non-HomePage files removed to fix build module not found error

--- src/App.js
+++ src/App.js
@@ -4,31 +4,20 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import MembershipPage from './pages/MembershipPage';
-import ParkingPage from './pages/ParkingPage';
-import NotFoundPage from './pages/NotFoundPage';
-import NewsPage from './pages/NewsPage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 
 import './App.css';
 
 const App = () => {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/membership" element={<MembershipPage />} />
-        <Route path="/parking" element={<ParkingPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<HomePage />} /> 
+            </Routes>
+            <Footer />
+        </Router>
+    );
 };
 
 export default App;
