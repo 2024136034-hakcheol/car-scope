41b262344a0f605f5e2bd89e9f4a66174e753680
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-13 21:08:47 +0900

    final: 모든 파일에서 주석 제거 및 Footer 최종 수정사항 반영 (회사 정보 삭제, 색상 통일, 빌드 오류 해결)

--- src/App.js
+++ src/App.js
@@ -4,31 +4,22 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import './App.css'; 
+import ParkingPage from './pages/ParkingPage';
 
-const App = () => {
-    return (
-        <Router>
-            <Header />
-            <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/news" element={<HomePage />} />
-                    <Route path="/parking" element={<HomePage />} />
-                    <Route path="/membership" element={<HomePage />} />
-                    <Route path="/faq" element={<HomePage />} />
-                    <Route path="/contact" element={<HomePage />} />
-                    <Route path="/privacy" element={<HomePage />} />
-                    <Route path="/terms" element={<HomePage />} />
-                    <Route path="/emergency" element={<HomePage />} />
-                    <Route path="/register" element={<HomePage />} />
-                    <Route path="/find-password" element={<HomePage />} />
-                </Routes>
-            </main>
-            <Footer />
-        </Router>
-    );
-};
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/parking" element={<ParkingPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
