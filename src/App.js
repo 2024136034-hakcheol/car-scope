1fd45f07d1d000eca24ba421091e5fc8e224a28c
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-13 18:36:14 +0900

    feat: 주석을 완전히 제거하고 로그인 페이지(LoginPage.js, LoginPage.css) 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css'; 
@@ -11,6 +12,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
             <Footer />
         </Router>
