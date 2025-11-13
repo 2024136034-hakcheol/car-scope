d008514600a91728d4ea3496ac5f31c68a70a0f0
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-13 16:37:44 +0900

    fix: Renamed styles folder to cssModules to force CSS module loading

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/HomePage.module.css'; 
+import styles from '../cssModules/HomePage.module.css';
 
 const generateStars = (rating) => {
     let stars = '';
