be60b74ebd675fa8427589d61b1f61cf522adf9d
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-17 15:29:14 +0900

    fix: GuestRoute가 auth 로딩 상태를 확인하도록 수정

--- src/components/GuestRoute.js
+++ src/components/GuestRoute.js
@@ -3,7 +3,11 @@ import { AuthContext } from '../AuthContext';
 import { Navigate } from 'react-router-dom';
 
 const GuestRoute = ({ children }) => {
-    const { currentUser } = useContext(AuthContext);
+    const { currentUser, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return null;
+    }
 
     if (currentUser) {
         alert("이미 로그인 중 입니다 로그아웃 하여 이용해 주십시오");
