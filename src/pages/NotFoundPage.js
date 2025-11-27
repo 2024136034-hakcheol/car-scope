fb1a57b8485c5109ed8be0507beb4c7b0da23401
2024136034-hakcheol <gkrcjf1019@g.shingu.ac.kr>
2025-11-27 14:16:22 +0900

    style: HomePage 배너 상단 밀착 및 하단 섹션 카드형 디자인 통일

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
         <div className="not-found-container">
             <div className="not-found-content">
                 <h1>404</h1>
-                <h2>주차장인지 확인</h2>
+                <h2>페이지를 찾을 수 없습니다.</h2>
                 <p>요청하신 페이지가 존재하지 않거나, 주소가 변경되었을 수 있습니다.</p>
                 <Link to="/" className="not-found-button">
                     홈으로 돌아가기
